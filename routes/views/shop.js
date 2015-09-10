0337134c810cae32270f7a88b05647e342284846
Arthur <zyujiawei@hotmail.com>
2015-09-10 17:01:51 +0800

    item page v2

--- routes/views/shop.js
+++ routes/views/shop.js
@@ -21,18 +21,18 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 
 	view.on('init', function(next) {
-		keystone.list('Series').model.findOne()
-		.where('name', series)
-		.exec(function(err, result) {
-			if (result == null){
-				console.log("result empty");
-			} else{
-				console.log(series);
-				console.log(result);
-			}
-			next(err);
-				// do something with posts
-		});
+		// keystone.list('Series').model.findOne()
+		// .where('name', series)
+		// .exec(function(err, result) {
+		// 	if (result == null){
+		// 		console.log("result empty");
+		// 	} else{
+		// 		console.log(series);
+		// 		console.log(result);
+		// 	}
+		// 	next(err);
+		// 		// do something with posts
+		// });
 
 		keystone.list('Product').paginate({
 				page: req.query.page || 1,
