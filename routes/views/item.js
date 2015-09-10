d3e3f62574093cff6357793df8e00b2811b7407e
Arthur <zyujiawei@hotmail.com>
2015-09-10 17:00:23 +0800

    item page

--- routes/views/item.js
+++ routes/views/item.js
@@ -4,10 +4,23 @@ exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	var id = req.params.id;
+	locals.data = {
+		products: []
+	};
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
 	locals.section = 'home';
+	view.on('init', function(next) {
+		keystone.list('Product').model.findOne()
+    .where('_id', id)
+    .populate('Series')
+    .exec(function(err, results) {
+			locals.data.products = results;
+			console.log(locals.data.products);
+			next(err);
+        // do something with posts
+    });
+	});
 
 	// Render the view
 	view.render('item');
--- routes/views/shop.js
+++ routes/views/shop.js
@@ -4,27 +4,47 @@ exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	var series = req.params.series;
+
+	switch (series){
+			case("all"):locals.section = '全';break;
+			case("Young"):locals.section = 'Young';break;
+			case("木质"):locals.section = '木质';break;
+	}
+
+
 	locals.data = {
 		products: []
 	};
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
-	locals.section = 'home';
+
 	view.on('init', function(next) {
-		var q = keystone.list('Product').paginate({
+		keystone.list('Series').model.findOne()
+		.where('name', series)
+		.exec(function(err, result) {
+			if (result == null){
+				console.log("result empty");
+			} else{
+				console.log(series);
+				console.log(result);
+			}
+			next(err);
+				// do something with posts
+		});
+
+		keystone.list('Product').paginate({
 				page: req.query.page || 1,
 				perPage: 10,
 				maxPages: 10
 			})
 			.populate('Series')
-			.sort('-publishedDate');
-
-		q.exec(function(err, results) {
-			console.log(results);
-			locals.data.products = results;
-			next(err);
-		});
+			.sort('-publishedDate')
+			.exec(function(err, results) {
+				locals.data.products = results;
+				next(err);
+			});
 	});
 	// Render the view
 	view.render('shop');
